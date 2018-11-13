718773f982a1b40516e4fd273691b0a97979f35d
Mohamed Assem <mo7mad.assim@gmail.com>
2018-11-13 22:50:14 +0200

    Add user map

--- app/src/main/java/app/pharmacy/map/com/mappharmacyapp/Activities/UserMapActivity.java
+++ app/src/main/java/app/pharmacy/map/com/mappharmacyapp/Activities/UserMapActivity.java
@@ -1,50 +1,190 @@
 package app.pharmacy.map.com.mappharmacyapp.Activities;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Objects;
 
 import app.pharmacy.map.com.mappharmacyapp.R;
 import butterknife.ButterKnife;
 
 public class UserMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private final String TAG = UserMapActivity.class.getSimpleName();
+    private static final float DEFAULT_ZOOM = 15f;
+    private boolean mLocationPermissionGranted = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private GoogleMap mMap;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_map);
         ButterKnife.bind(this);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        init();
     }
 
+    private void init() {
+        getLocationPermission();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true); //to get blue marker with GPS icon
+            mMap.getUiSettings().setMyLocationButtonEnabled(true); //to hide GPS icon
+            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    if (marker != null) { //if marker exists (not null or whatever)
+                        marker.setPosition(latLng);
+                    } else {
+                        marker = mMap.addMarker(new MarkerOptions()
+                                .position(latLng)
+                                .title("test")
+                                .draggable(true));
+                    }
+                }
+            });
+        }
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    // Assem
+    private void sendDataBack(LatLng latLng) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("lng", latLng.longitude);
+        returnIntent.putExtra("lat", latLng.latitude);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+
+
+    // Assem
+    public void customMarker(Context context, GoogleMap mMap, int drawable, String title, LatLng latLng) {
+        int height = 80;
+        int width = 80;
+        BitmapDrawable bitmapDraw = (BitmapDrawable) context.getResources().getDrawable(drawable);
+        Bitmap b = bitmapDraw.getBitmap();
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        MarkerOptions mMarkerOptions = new MarkerOptions();
+        mMarkerOptions.title(title).position(latLng)
+                .icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+        mMap.addMarker(mMarkerOptions);
+    }
+
+    private void initMAP() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        Objects.requireNonNull(mapFragment).getMapAsync(UserMapActivity.this);
+    }
+
+    private void getDeviceLocation() {
+        FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if (mLocationPermissionGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Location current_location = (Location) task.getResult();
+                            try {
+                                moveCamera(new LatLng(current_location.getLatitude(), current_location.getLongitude()), DEFAULT_ZOOM);
+
+                            } catch (Exception e) {
+                                onBackPressed();
+                                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                Toast.makeText(UserMapActivity.this, getString(R.string.enable_premissions), Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(UserMapActivity.this, getString(R.string.enable_premissions), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                Log.e(TAG, "getDeviceLocation: else");
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: " + e.getMessage());
+        }
+    }
+
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void getLocationPermission() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+                initMAP();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                    //initialize our map
+                    initMAP();
+                }
+            }
+        }
     }
 }
